1088e52c12ed3cbb6d436acc101a241fbc6cc15b
bogle-zhao <bogle.zhao@foxmail.com>
2022-01-10 13:38:06 +0800

    Changes

---
+++ src/app/websocket/websocket-server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const io = require('socket.io');
+const fs = require('fs');
+
+const address = 'localhost';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    let url = req.url;
+    if (url === '/') {
+        fs.readFile('./client.html', 'utf-8', (error, data) => {
+            if (error) {
+                console.error('error occured');
+                return
+            }
+            // 将读取的网页返回给客户端
+            res.end(data.toString);
+        });
+    } else {
+        res.end('<html><body>Error</body></html>');
+    }
+});
+
+server.listen(3000, address);
+// 将server进行包装，转换为webSocket
+const webSocket = io.listen(server);
+
+webSocket.on('connection', (socket) => {
+    console.log('connection has been established');
+
+    socket.on('message', (message) => {
+        console.log('message: ' + message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('connection has lost');
+    });
+
+    socket.send('hello client');
+});
+
+
